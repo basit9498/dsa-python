6acc9b3dd46fa0139840d1e0b2c04d043f80c972
Abdul Basit <a.basit@mobiotransfers.com>
2024-07-23 11:47:45 +0500

    Find Middle of the Linked List

--- LinkList.py
+++ LinkList.py
@@ -1,3 +1,6 @@
+import math
+
+
 class Node:
     def __init__(self, data):
         self.data = data
@@ -12,7 +15,6 @@ class SignleLinkList:
         current_node = self.head
         if current_node:
             while current_node:
-                print(current_node.next)
                 if current_node.next:
                     current_node = current_node.next
                 else:
@@ -27,9 +29,36 @@ class SignleLinkList:
             print(current_node.data)
             current_node = current_node.next
 
+    def find_middle(self):
+        arr = []
+        current_node = self.head
+        while current_node:
+            arr.append(current_node.data)
+            current_node = current_node.next
+
+        arr_len = len(arr)
+        print(arr_len)
+        if arr_len % 2 == 0:
+            print("Even number in length")
+            middle_index=int(arr_len/2)
+            print("two middle:",arr[middle_index-1],arr[middle_index])
+        else:
+            print("Odd number in length")
+            index=math.ceil(arr_len / 2)
+            print("Middle:",index )
+            print("Value is:",arr[index-1])
+
 
 LinkList = SignleLinkList()
-LinkList.append_node(Node(1))
-LinkList.print_list()
 LinkList.append_node(Node(2))
-LinkList.print_list()
+LinkList.append_node(Node(4))
+LinkList.append_node(Node(1))
+LinkList.append_node(Node(5))
+LinkList.append_node(Node(6))
+LinkList.append_node(Node(9))
+LinkList.append_node(Node(8))
+LinkList.append_node(Node(7))
+LinkList.append_node(Node(10))
+
+LinkList.find_middle()
+# LinkList.print_list()
